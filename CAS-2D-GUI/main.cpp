d8310d1d307c73b73d654a21a9698e2a9e84565c
Mimis <dim_kar@outlook.com>
2026-03-01 17:45:40 +0200

    apply correctly the font

--- CAS-2D-GUI/main.cpp
+++ CAS-2D-GUI/main.cpp
@@ -1,21 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QFile>
+#include <QFont>
 #include <QFontDatabase>
 #include <QIcon>
 #include <QSize>
-#include <QString>
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/assets/gpuopen.ico"));
-
+    // apply the stylesheet
     QFile styleFile(":/stylesheets/main.qss");
     styleFile.open(QFile::ReadOnly);
-    // Apply the stylesheet
     a.setStyleSheet(QString(styleFile.readAll()));
-    QFontDatabase::addApplicationFont(":/assets/fonts/TitilliumWeb-Regular.ttf");
-
+    // apply the font
+    const int fontId = QFontDatabase::addApplicationFont(":/assets/fonts/TitilliumWeb-Regular.ttf");
+    if (fontId != -1)
+        a.setFont(QFont(QFontDatabase::applicationFontFamilies(fontId).at(0), 10));
+    // show the window
     MainWindow w;
     w.resize(QSize(320, 160));
     w.show();
